eb20b8e306e068d76488b9f44fc9f763d87d6125
Matthew Freeman <matthew.freeman@revature.net>
2021-03-30 10:05:18 -0400

    Adds playGame method with basic print line and calls method in main method

---
+++ main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        playGame();
+    }
+
+    public static void playGame() {
+        //Add basic logic to play game here
+        System.out.println("You are playing the game");
+    }
+}
