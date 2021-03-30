a0be4f4e61c13f1c9f0616fa571c8f9f5f6d79b0
Matthew Freeman <matthew.freeman@revature.net>
2021-03-30 13:52:12 -0400

    Adds logic to ask user to continue playing or not and will exit the game based on choice made

--- main/java/App.java
+++ main/java/App.java
@@ -1,10 +1,33 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         playGame();
     }
 
     public static void playGame() {
+        Scanner sc = new Scanner(System.in);
+        boolean playing = true;
         //Add basic logic to play game here
-        System.out.println("You are playing the game");
+        while(playing){
+            System.out.println("Would you like to continue playing?");
+            System.out.println("1: yes");
+            System.out.println("2: no");
+            int choice = sc.nextInt();
+            System.out.println(choice);
+            if(choice == 1){
+                System.out.println("You are playing the game");
+                System.out.println("Would you like to continue playing?");
+                System.out.println("1: yes");
+                System.out.println("2: no");
+                choice = sc.nextInt();
+            } else {
+                System.out.println("You quit the game");
+                playing = false;
+                sc.close();
+            }
+        }
     }
 }
