86523cda282971d18d63b267f1204a2bdc266cd8
Matthew Freeman <matthew.freeman@revature.net>
2021-04-02 13:01:44 -0400

    Adds methods for main play of game. Need to fix player choosing option 3 to leave the game. Currently works, but is hacky

--- main/java/App.java
+++ main/java/App.java
@@ -5,14 +5,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    static Scanner sc = new Scanner(System.in);
+    static Random randomNumber = new Random();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random randomNumber = new Random();
-
-        String[] furniture = {"Chest", "Bookcase"};
-        String[] collectibles = {"Gem", "Ring", "Necklace"};
+        //Make class for player, include name and bag?
         ArrayList<String> bag = new ArrayList<>();
-        int findItemChance = 66;
 
         System.out.println("###################################################################");
         System.out.println("###############     Welcome to the Castle     #####################");
@@ -22,10 +19,18 @@ public class App {
         System.out.println("\tSearch these pieces to find collectible items.");
         System.out.println("\tPlace the collectibles in your bag.");
         System.out.println("\tCollect three items to win!");
+
         boolean searching = true;
 
-        while(searching) {
-            while(bag.size() < 3){
+        gamePlay(searching, bag);
+    }
+
+    public static void gamePlay(boolean playing, ArrayList<String> pack){
+        while(playing) {
+            //Make classes for items and bag?
+            String[] furniture = {"Chest", "Bookcase"};
+
+            while(pack.size() < 3){
                 System.out.println("-------------------------------------------------------------------");
     
                 String searchableItem = furniture[randomNumber.nextInt(furniture.length)];
@@ -35,47 +40,65 @@ public class App {
                 System.out.println("\t2. Leave it alone and keep looking");
                 System.out.println("\t3. Leave the castle");
                 String input = sc.nextLine();
-                if(input.equals("1")) {
-                    if(randomNumber.nextInt(100) < findItemChance) {
-                        String foundItem = collectibles[randomNumber.nextInt(collectibles.length)];
-                        System.out.println("\tYou found a " + foundItem + "!");
-                        System.out.println("\tWhat would you like to do?");
-                        System.out.println("\t1. Put the item in my bag.");
-                        System.out.println("\t2. Leave it where it is.");
-                        String choice = sc.nextLine();
-                        if(choice.equals("1")) {
-                            bag.add(foundItem);
-                            System.out.println("You put the " + foundItem + " in your bag!");
-                            System.out.println("You now have " + bag.size() + " item(s) in your bag.");
-                            System.out.println("You need " + (3 - bag.size()) + " more items to win!");
-                        } else {
-                            System.out.println("You leave the " + foundItem + " alone.");
-                            System.out.println("You have " + bag.size() + " item(s) in your bag.");
-                            System.out.println("You need " + (3 - bag.size()) + " more items to win!");
-                            continue;
-                        }
-                    } else {
-                        System.out.println("The " + searchableItem + " was empty!");
-                        continue;
-                    }
-                } else if(input.equals("2")){
-                    System.out.println("You leave the " + searchableItem + " alone and keep searching.");
-                    continue;
-                } else if(input.equals("3")) {
-                    System.out.println("You decide to leave the castle without filling your bag.");
-                    break;
-                } else {
-                    System.out.println("Invlaid command!");
-                }
+                
+                playerChoice(input, pack, searchableItem);
             }
             System.out.println("###################################################################");
             System.out.println("################     Thanks for playing!     ######################");
             System.out.println("###################################################################");
-            searching = false;
+            playing = false;
             
             sc.close();
         }
+    }
 
+    public static void playerChoice(String choice, ArrayList<String> pack, String pieceOfFurniture){
+
+        String[] collectibles = {"Gem", "Ring", "Necklace"};
+        int findItemChance = 66;
+
+        if(choice.equals("1")) {
+            if(randomNumber.nextInt(100) < findItemChance) {
+                String foundItem = collectibles[randomNumber.nextInt(collectibles.length)];
+                System.out.println("\tYou found a " + foundItem + "!");
+                System.out.println("\tWhat would you like to do?");
+                System.out.println("\t1. Put the item in my bag.");
+                System.out.println("\t2. Leave it where it is.");
+                String nextChoice = sc.nextLine();
+
+                while(!nextChoice.equals("1") && !nextChoice.equals("2")) {
+                    System.out.println("Invalid command!");
+                    System.out.println("\tWhat would you like to do?");
+                    System.out.println("\t1. Put the item in my bag.");
+                    System.out.println("\t2. Leave it where it is.");
+                    choice = sc.nextLine();
+                }
+                if(nextChoice.equals("1")) {
+                    pack.add(foundItem);
+                    System.out.println("You put the " + foundItem + " in your bag!");
+                    System.out.println("You now have " + pack.size() + " item(s) in your bag.");
+                    System.out.println("You need " + (3 - pack.size()) + " more items to win!");
+                } else {
+                    System.out.println("You leave the " + foundItem + " alone.");
+                    System.out.println("You have " + pack.size() + " item(s) in your bag.");
+                    System.out.println("You need " + (3 - pack.size()) + " more items to win!");
+                    // continue;
+                }
+            } else {
+                System.out.println("The " + pieceOfFurniture + " was empty!");
+                // continue;
+            }
+        } else if(choice.equals("2")){
+            System.out.println("You leave the " + pieceOfFurniture + " alone and keep searching.");
+            // continue;
+        } else if(choice.equals("3")) {
+            System.out.println("You decide to leave the castle without filling your bag.");
+            pack.add(" ");
+            pack.add(" ");
+            pack.add(" ");
+        } else {
+            System.out.println("Invlaid command!");
+        }
     }
 
 }
