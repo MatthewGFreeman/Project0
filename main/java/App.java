bb93a419de21d6f986dfe4d06a139e7d5e23f3e3
Matthew Freeman <matthew.freeman@revature.net>
2021-04-02 10:48:07 -0400

    Adds some feedback for the user

--- main/java/App.java
+++ main/java/App.java
@@ -17,8 +17,15 @@ public class App {
         int gemDropChance = 33;
         int ringDropChance = 50;
         int necklaceropChance = 50;
-        //New tag line needed
-        System.out.println("Welcome to the Castle!");
+        //New tag line needed and cooler formatting please
+        System.out.println("##########################");
+        System.out.println("  Welcome to the Castle   ");
+        System.out.println("##########################");
+        System.out.println("\tYour goal is to collect three items.");
+        System.out.println("\tAs you search the castle you will come across some furniture.");
+        System.out.println("\tSearch these pieces to find collectible items.");
+        System.out.println("\tPlace the collectibles in your bag.");
+        System.out.println("\tCollect three items to win!");
         boolean searching = true;
         // GAME:
         while(searching) {
@@ -36,7 +43,7 @@ public class App {
                 if(input.equals("1")) {
                     if(randomNumber.nextInt(100) < findItemChance) {
                         String foundItem = collectibles[randomNumber.nextInt(collectibles.length)];
-                        System.out.println("You found a " + foundItem + "!");
+                        System.out.println("\tYou found a " + foundItem + "!");
                         System.out.println("\tWhat would you like to do?");
                         System.out.println("\t1. Put the item in my bag.");
                         System.out.println("\t2. Leave it where it is.");
@@ -44,8 +51,12 @@ public class App {
                         if(choice.equals("1")) {
                             bag.add(foundItem);
                             System.out.println("You put the " + foundItem + " in your bag!");
+                            System.out.println("You now have " + bag.size() + " item(s) in your bag.");
+                            System.out.println("You need " + (3 - bag.size()) + " more items to win!");
                         } else {
                             System.out.println("You leave the " + foundItem + " alone.");
+                            System.out.println("You have " + bag.size() + " item(s) in your bag.");
+                            System.out.println("You need " + (3 - bag.size()) + " more items to win!");
                             continue;
                         }
                     } else {
@@ -53,8 +64,10 @@ public class App {
                         continue;
                     }
                 } else if(input.equals("2")){
+                    System.out.println("You leave the " + searchableItem + " alone and keep searching.");
                     continue;
                 } else if(input.equals("3")) {
+                    System.out.println("You decide to leave the castle without filling your bag.");
                     break;
                 } else {
                     System.out.println("Invlaid command!");
